52bf2cb53e935bf1d045950d505c6f2a188778fe
Stanislau Bandarchuk <desudesu410@gmail.com>
2020-09-30 18:44:41 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) {
+  return ((array == null) || (array.length === 0)) ? 0 : Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array) {
+  return ((array == null) || (array.length === 0)) ? 0: Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array) {
+  let sum = 0;
+  if ((array == null) || (array.length === 0)) 
+    return 0;
+
+  for (let i = 0; i < array.length; i += 1)
+    sum += array[i];
+  return sum / array.length;
 }
