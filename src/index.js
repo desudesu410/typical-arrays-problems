6ab213ea3adb84f0fec873e5a5cd39eb3b0a74a5
Stanislau Bandarchuk <desudesu410@gmail.com>
2021-03-04 02:23:20 +0300

    refactor: improve code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 exports.min = function min (array) {
-  return ((array == null) || (array.length === 0)) ? 0 : Math.min.apply(null, array);
+  return (!array || array.length === 0) 
+    ? 0 
+    : Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return ((array == null) || (array.length === 0)) ? 0: Math.max.apply(null, array);
+  return (!array || array.length === 0) 
+    ? 0 
+    : Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  let sum = 0;
-  if ((array == null) || (array.length === 0)) 
+  if (!array || array.length === 0) 
     return 0;
-
-  for (let i = 0; i < array.length; i += 1)
+  let sum = 0;
+  let avg = 0;
+  for (let i = 0; i < array.length; i++)
     sum += array[i];
-  return sum / array.length;
+  avg = sum / array.length;
+  return avg;
 }
